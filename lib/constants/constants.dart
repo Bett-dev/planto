4543589082ee788306097f1a8dbae7e281884f7a
Bett-dev <bettkipkiruijustus@gmail.com>
2025-02-15 22:02:24 +0300

    update

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const kLogoTextStyle = TextStyle(
   fontSize: 28,
-  fontWeight: FontWeight.w600,
+  fontWeight: FontWeight.w900,
   color: Color(0xffffffff),
 );
 
